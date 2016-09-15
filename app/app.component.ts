ef3f39705fcf7e7379503b3d1796200b2a2ce05b
Anil Shrestha <Anil.Shrestha@medeanalytics.com>
2016-09-15 00:07:09 -0700

    Entry Point for the applicaiton

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1> Your Angular 2 Starter Kit was Successfull</h1>'	
+})
+export class AppComponent { }
